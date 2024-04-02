8a3de2cb4543063f4c3a07ed1510932732d70d52
mahmudhmh <mahmoudh.buss@gmail.com>
2024-04-02 05:00:49 +0200

    feat: added home screen with static structure.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -11,26 +11,31 @@
         <link rel="preconnect" href="https://fonts.bunny.net">
         <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
+        <style>
+            [x-cloak]{
+                display: none !important;
+            }
+        </style>
         <!-- Scripts -->
         @vite(['resources/css/app.css', 'resources/js/app.js'])
     </head>
     <body class="font-sans antialiased">
-        <div class="min-h-screen bg-gray-100 dark:bg-gray-900">
-            @include('layouts.navigation')
 
-            <!-- Page Heading -->
-            @if (isset($header))
-                <header class="bg-white dark:bg-gray-800 shadow">
-                    <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                        {{ $header }}
-                    </div>
-                </header>
-            @endif
+        <div class="drawer lg:drawer-open">
+            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+            <div class="drawer-content items-center justify-center">
+              <!-- Page content here -->
+              {{-- <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label> --}}
+                {{$slot}}
+            </div>
+            <div class="drawer-side overflow-visible z-10">
+              <label for="my-drawer-2" class="drawer-overlay"></label>
 
-            <!-- Page Content -->
-            <main>
-                {{ $slot }}
-            </main>
-        </div>
+              @include('layouts.sidebar')
+
+            </div>
+          </div>
+
+
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
+use App\Livewire\Home;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,9 +15,7 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', Home::class);
 
 Route::get('/dashboard', function () {
     return view('dashboard');
