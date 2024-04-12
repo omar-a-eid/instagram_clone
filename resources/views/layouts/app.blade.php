a6f2e0753c408d7ac2b2db850e262c0d18409f8b
mahmudhmh <mahmoudh.buss@gmail.com>
2024-04-12 15:01:52 +0200

    feat: Implemented Search function in the Sidebar * feat: Skeleton Working on Notifications

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,42 +1,42 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <style>
-            [x-cloak]{
-                display: none !important;
-            }
-        </style>
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-    </head>
-    <body class="font-sans antialiased">
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-        <div class="drawer lg:drawer-open">
-            <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
-            <div class="drawer-content items-center justify-center">
-              <!-- Page content here -->
-              {{-- <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label> --}}
-                {{$slot}}
-            </div>
-            <div class="drawer-side overflow-visible z-10">
-              <label for="my-drawer-2" class="drawer-overlay"></label>
+    <style>
+        [x-cloak] {
+            display: none !important;
+        }
+    </style>
+    <!-- Scripts -->
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
+</head>
 
-              @include('layouts.sidebar')
+<body class="font-sans antialiased">
 
-            </div>
-          </div>
+    <div class="drawer lg:drawer-open">
+        <input id="my-drawer-2" type="checkbox" class="drawer-toggle" />
+        <div class="drawer-content items-center justify-center">
+            <!-- Page content here -->
+            {{-- <label for="my-drawer-2" class="btn btn-primary drawer-button lg:hidden">Open drawer</label> --}}
+            {{ $slot }}
+        </div>
+        <div class="drawer-side overflow-visible z-10">
+            <label for="my-drawer-2" class="drawer-overlay"></label>
 
-              @livewire('wire-elements-modal')
+            {{-- @include('layouts.sidebar') --}}
+            <livewire:components.sidebar />
+        </div>
+        @livewire('wire-elements-modal')
+</body>
 
-    </body>
 </html>
